5a6f6ef48131a0efbfe15f9f253762968c1693cf
rsslbd <110036786+rsslbd@users.noreply.github.com>
2022-12-23 18:06:22 +0600

    Update EmployeeListForm.java

--- src/com/idb/gui/EmployeeListForm.java
+++ src/com/idb/gui/EmployeeListForm.java
@@ -210,6 +210,7 @@ public class EmployeeListForm extends javax.swing.JFrame {
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run() {
                 new EmployeeListForm().setVisible(true);
             }
